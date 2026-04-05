f89434ec529475c7c2ca73a48da22914faa74926
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-04-05 11:53:30 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+////////////////////////
+////////////////////////
+////////////////////////
+////////////////////////
+////////////////////////
+
+// selecting elements
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+
+const allSection = document.querySelectorAll('.section');
+// console.log(allSection);
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
+
+// insertAdjacentHTML
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+// message.textContent =
+//   'We use cookied for improved functionality and analytics.';
+
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// delete element
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.parentElement.removeChild(message));
+
+//styles
+
+message.style.backgroundColor = '#37383d';
+
+message.style.width = '120%';
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).height);
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// atributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.className);
+logo.alt = 'minimalist logo';
+console.log(logo.alt);
+logo.setAttribute('company', 'bankist');
+console.log(logo.getAttribute('company'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// data
+
+console.log(logo.dataset.versionNumber);
+
+// classes
+
+logo.classList.add('c', 'k');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+// dont's use
+// logo.className = 'jonas';
+
+console.log(logo.classList);
