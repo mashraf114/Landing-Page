3d568c8e000ada0bde933f65df4742c48967e5bf
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-04-07 06:52:31 +0200

    feat: Revealing Elements on Scroll

--- script.js
+++ script.js
@@ -163,11 +163,11 @@ const handleHover = function (e) {
 
 const header = document.querySelector('header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -179,6 +179,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+//////////////////////////////////
+// reveal sction
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  // gaurd clause
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 ////////////////////////
 ////////////////////////
 ////////////////////////
